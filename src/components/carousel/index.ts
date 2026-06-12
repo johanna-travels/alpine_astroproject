0a13f5942a3d147190ef624fe20de92993b4b3ec
Mike Pap <voyaflair@gmail.com>
2026-06-12 14:15:28 +0200

    add-scroll-effect

--- src/components/carousel/index.ts
+++ src/components/carousel/index.ts
@@ -3,6 +3,4 @@
  * Import from '@/components/carousel' instead of reaching into individual files,
  * so internal file renames never break consumers.
  */
-export { Carousel, Card, CarouselContext, BlurImage } from "./apple-cards-carousel";
 export { default as CarouselDots } from "./CarouselDots";
-export { default as AppleCardsCarouselDemo } from "./apple-cards-carousel-demo";
--- tailwind.config.js
+++ tailwind.config.js
@@ -35,5 +35,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('lightswind/plugin'),],
 };
