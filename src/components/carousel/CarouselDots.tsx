562be32db0a48ef44d6ef04bca3aa713d720842e
Mike Pap <voyaflair@gmail.com>
2026-06-12 12:25:36 +0200

    fix carousel dots import

--- src/components/carousel/CarouselDots.tsx
+++ src/components/carousel/CarouselDots.tsx
@@ -1,4 +1,4 @@
- import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 interface CarouselDotsProps {
   carouselId: string;
